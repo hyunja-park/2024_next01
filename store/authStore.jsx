cceb5799c76324ba7e22bbe222e39b9a362c32f9
parkhyunja <hjpark20121125@gmail.com>
2024-12-09 18:02:26 +0900

    1209수정

--- store/authStore.jsx
+++ store/authStore.jsx
@@ -10,6 +10,7 @@ const useAuthStore = create(
 
             // 로그인 처리
             login: (user, token) => {
+                console.log("로그인 상태 업데이트:", user, token);
                 set({ user, token, isAuthenticated: true });
             },
 
